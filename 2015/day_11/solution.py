c457c778a737c812c4c23a38d0f4d007bd66ab17
Marcin Krezel <marcinkrezel94@outlook.com>
2024-12-29 22:25:41 +0100

    feat: 2015 11 fix

--- 2015/day_11/solution.py
+++ 2015/day_11/solution.py
@@ -1,6 +1,7 @@
-from collections import defaultdict
+from itertools import pairwise
 
-data = "hxbxwxba"
+# data = "hxbxwxba"
+data = "hxbxxyzz"
 
 
 def is_password_valid(s: str):
@@ -9,19 +10,52 @@ def is_password_valid(s: str):
     if any(val in res for val in [ord("i"), ord("o"), ord("l")]):
         return False
 
-    pairs = set([s[i:i + 2] for i in range(0, len(s), 2)])
+    pairs = set()
+
+    for i in range(len(s) - 1):
+        pair = s[i: i + 2]
+        if len(set(pair)) == 1:
+            pairs.add(pair)
+
     if len(pairs) < 2:
         return False
 
-    threes = [s[i: i + 3] for i in range(len(s) - 2)]
-    print(threes)
+    threes = [res[i: i + 3] for i in range(len(s) - 2)]
+    found = False
+    for three in threes:
+        if set([y - x for (x, y) in pairwise(three)]) == {1}:
+            found = True
+            break
+
+    if not found:
+        return False
 
     return True
 
 
-while not is_password_valid(data):
-    data_ = [ord(ch) for ch in data]
+def increment_string(c: str):
+    c = list(c)
+    for i in range(len(c) - 1, 0, -1):
+        if c[i] == "z":
+            c[i] = chr((ord(c[i]) - ord('a') + 1) % 26 + ord('a'))
+        else:
+            increment = 1
+            if c[i] in ("h", "n", "k"):
+                increment = 2
+            c[i] = chr((ord(c[i]) - ord('a') + increment) % 26 + ord('a'))
+            break
+    return "".join(c)
 
-    data = "".join([chr(num) for num in data_])
+
+# Part 1
+# while not is_password_valid(data):
+#     data = increment_string(data)
+#     print(data)
+
+# Part 2
+data = increment_string(data)
+while not is_password_valid(data):
+    data = increment_string(data)
+    # print(data)
 
 print(data)
