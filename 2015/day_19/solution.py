78648fc0a6f520fde78e21f04a1140ca294b6be5
Marcin Krezel <marcinkrezel94@outlook.com>
2025-01-05 17:23:07 +0100

    feat: 2015 19- 1 fix

--- 2015/day_19/solution.py
+++ 2015/day_19/solution.py
@@ -1,21 +1,54 @@
+import random
+
 from helpers.input_loader import load_input
 import re
+from random import shuffle
 
-_data = load_input().splitlines()
-text = _data[-1]
-rules = [d.split(" => ") for d in _data[:-2]]
-# print(rules)
-# print(text)
 
-values = set()
+def part_1():
+    _data = load_input().splitlines()
+    text = _data[-1]
+    rules = [d.split(" => ") for d in _data[:-2]]
+    # print(rules)
+    # print(text)
 
-for r in rules:
-    ch, replace_value = r
-    indexes = [m.start() for m in re.finditer(ch, text)]
+    values = set()
 
-    for index in indexes:
-        end_index = index + len(ch)
-        s = text[:index] + replace_value + text[end_index:]
-        values.add(s)
+    for r in rules:
+        ch, replace_value = r
+        indexes = [m.start() for m in re.finditer(ch, text)]
 
-print(len(values))
+        for index in indexes:
+            end_index = index + len(ch)
+            s = text[:index] + replace_value + text[end_index:]
+            values.add(s)
+
+    print(len(values))
+
+
+def part_2():
+    _data = load_input().splitlines()
+    text = _data[-1]
+    rules = [d.split(" => ")[::-1] for d in _data[:-2]]
+
+    target = text
+
+    count = 0
+    while target != 'e':
+        tmp = target
+        for a, b in rules:
+            if a not in target:
+                continue
+
+            target = target.replace(a, b, 1)
+            count += 1
+
+        if tmp == target:
+            target = text
+            count = 0
+            shuffle(rules)
+
+    return count
+
+
+print(part_2())
